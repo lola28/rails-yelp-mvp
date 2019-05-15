8058e75b5b57b26ad50c069c3bfa314bca3707a1
Frankie Ling <ingleson.fl@gmail.com>
2019-05-15 15:22:51 +0100

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+
+  before_action :get_restaurant, except: [:index, :create, :new]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant created'
+    else
+      render :new
+    end
+  end
+
+private
+
+  def get_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    category: 'french',
+    phone_number: '02234986670'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    category: 'chinese',
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+  {
+    name: 'McDonalds',
+    address: Faker::Address.street_address,
+    category: 'japanese',
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+  {
+    name: 'Pizza',
+    address: Faker::Address.street_address,
+    category: 'italian',
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+  {
+    name: 'Vegan',
+    address: Faker::Address.street_address,
+    category: 'belgium',
+    phone_number: Faker::PhoneNumber.phone_number
+  }
+]
+
+Restaurant.create(restaurants_attributes)
